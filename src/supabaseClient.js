c92f38db5365383ef3862fc3e397b8925ed4bcee
thouth <78444186+thouth@users.noreply.github.com>
2025-05-22 14:25:52 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://your-project.supabase.co";
-const supabaseAnonKey = "your-anon-key";
+const supabaseUrl = "https://vkrxbvxitrgjixovhsww.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrcnhidnhpdHJnaml4b3Zoc3d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MjIxNzcsImV4cCI6MjA2MzM5ODE3N30.XnqsVZg0nIUO4E9p_FwlDUXfz0gr5tXZcXQ0SQv2928";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
